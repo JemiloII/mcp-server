5d745da544d7ea98a363c045ca39a89c8fb2e82a
Brian Jemilo II <jemiloii@gmail.com>
2025-11-18 09:25:44 -0600

    Fix filter

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,7 +3,7 @@ import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 
 export default async function registerTools (mcp: McpServer) {
   const dir = await readdir(new URL('.', import.meta.url));
-  for (const module of dir.filter(m => m !== 'index.ts' && m === 'shared')) {
+  for (const module of dir.filter(m => m !== 'index.ts' && m !== 'shared')) {
     const { name, tool, callback, disabled } = await import(`./${module}`);
     if (disabled) continue;
     console.log('Registering Tool:', tool.title);
